9ba2b757d576ceb54a443229ab3f1e08bc555fc8
armangorjipoor <armangorjipoor@gmail.com>
2021-02-18 14:53:28 +0330

    initial project

---
+++ PUSHTOGIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PUSHTOGIT
+//
+//  Created by ArmanG on 11/30/1399 AP.
+//  Copyright © 1399 Pars Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
